d85e35f2d86bce9c34754854c5b472f4148eca51
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2023-12-01 19:34:09 +0200

    last-tasks

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const fontSizeControl = document.getElementById("font-size-control");
+const textElement = document.getElementById("text");
+
+fontSizeControl.addEventListener("input", updateFontSize);
+
+function updateFontSize() {
+  const fontSizeValue = fontSizeControl.value;
+  textElement.style.fontSize = `${fontSizeValue}px`;
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const bodyElement = document.body;
+const colorSpan = document.querySelector(".color");
+const colorchangeButton = document.querySelector(".change-color");
+
+colorchangeButton.addEventListener("click", changeBodyColor);
+
+function changeBodyColor() {
+  const randomColor = getRandomHexColor();
+
+  bodyElement.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
