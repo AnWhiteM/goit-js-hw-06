a3855bfc2367327d43b9e58ca768c26b5941a447
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2023-12-01 21:59:01 +0200

    1st-task

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,8 @@ const categoriesList = document.getElementById("categories");
 const categoryItems = categoriesList.querySelectorAll("li.item");
 console.log("Number of categories:", categoryItems.length);
 categoryItems.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
-  const categoryElements = category.querySelectorAll("ul li");
+  const categoryName = category.firstElementChild.textContent;
+  const categoryElements = category.lastElementChild.children;
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements.length}`);
 });
